62f4c882a22c96adda1b6891f4a84da9250171a2
Aleeza Jogiyat <alizajogyat09@gmail.com>
2022-01-17 15:20:14 +0500

    Done

--- src/components/tempApp/TempApp.js
+++ src/components/tempApp/TempApp.js
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.scss";
 
 export const TempApp = () => {
-  const [input, setInput] = useState();
-  console.log("nfn", input);
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState(null);
+  const apiKey = "91c05bf13a4f90b0681fa31bd8db0227";
 
   const handleChange = (e) => {
-    setInput(e.target.value);
+    setSearch(e.target.value);
   };
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=${apiKey}`;
+      const response = await fetch(url);
+      const weatherRes = await response.json();
+      setData(weatherRes.main);
+      // console.log("kbd", res);
+    };
+    fetchApi();
+  }, [search]);
+
   return (
     <>
       <div className="box">
@@ -15,22 +28,28 @@ export const TempApp = () => {
           <input
             type="search"
             name="search"
+            value={search}
             className="inputField"
-            value={input}
             onChange={handleChange}
           ></input>
         </div>
-        <div className="information">
-          <h2 className="heading">
-            <i class="fas fa-street-view"></i>
-            Pune
-          </h2>
-          <h1 className="temperature">52.5</h1>
-          <h3 className="max_temp">Min:50 | Max:52</h3>
-        </div>
-        <div className="wave-one"></div>
-        <div className="wave-two"></div>
-        <div className="wave-three"></div>
+        {!data ? (
+          <p>No data found</p>
+        ) : (
+          <div>
+            <div className="information">
+              <h2 className="heading">
+                <i className="fas fa-street-view"></i>
+                {search}
+              </h2>
+              <h1 className="temperature">{data.temp}°C</h1>
+              <h3 className="max_temp">
+                Min:{data.temp_min}°C | Max:{data.temp_max}°C
+              </h3>
+            </div>
+            <div className="wave-one"></div>
+          </div>
+        )}
       </div>
     </>
   );
